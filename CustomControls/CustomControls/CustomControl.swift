4761f7f7bb3fa96eee6ad2c87c300886f9adad92
Frulwinn <frulwinn@gmail.com>
2019-01-15 13:03:40 -0800

    added 4 component constatns to CustomControl

--- CustomControls/CustomControls/CustomControl.swift
+++ CustomControls/CustomControls/CustomControl.swift
@@ -3,4 +3,9 @@ import UIKit
 class CustomControl: UIControl {
     
     var value: Int = 1
+    let componentDimension: CGFloat = 40.0
+    let componentCount = 5
+    let componentActiveColor = UIColor.black
+    let componentInactiveColor = UIColor.gray
+    
 }
