16af103e3fe64a5c7a4aa66e3dd92c7c5a95fa60
Frulwinn <frulwinn@gmail.com>
2019-01-15 13:45:37 -0800

    created private func setup to make my labels

--- CustomControls/CustomControls/CustomControl.swift
+++ CustomControls/CustomControls/CustomControl.swift
@@ -3,6 +3,8 @@ import UIKit
 class CustomControl: UIControl {
     
     var value: Int = 1
+    var ratingLabels = [UILabel]()
+    
     let componentDimension: CGFloat = 40.0
     let componentCount = 5
     let componentActiveColor = UIColor.black
@@ -13,7 +15,39 @@ class CustomControl: UIControl {
         setup()
     }
     
-    func setup() {
+    private func setup() {
+        var count = 0
         
+        //loop to create 5 labels with each label's frame to componentDimension by componentDimension
+        //layout labels in a row with 8 spacing
+
+        for number in 1...5 {
+            let label = UILabel(frame: CGRect(x: 8, y: 0, width: componentDimension, height: componentDimension))
+         
+        //append
+            self.addSubview(label)
+            
+        //add tag to represent which star it is
+            label.text = "☆"
+            label.tag = number
+            ratingLabels.append(label)
+            count += 1
+            
+        //font bold system font, size 32.0
+            label.font = UIFont.boldSystemFont(ofSize: 32.0)
+            
+        //set unicode star and align center for label
+            label.textAlignment = .center
+            
+        //set label's text color
+            label.textColor = componentInactiveColor
+        }
+    }
+        override var intrinsicContentSize: CGSize {
+            let componentsWidth = CGFloat(componentCount) * componentDimension
+            let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+            let width = componentsWidth + componentsSpacing
+            return CGSize(width: width, height: componentDimension)
+        }
     }
 }
